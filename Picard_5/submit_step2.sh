f70a63a5a3ea950d6c3ac75b5f740726177c2e0e
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-05-19 10:20:07 +0200

    more Memory less threads

--- Picard_5/submit_step2.sh
+++ Picard_5/submit_step2.sh
@@ -7,6 +7,6 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=128
 #SBATCH --time=2-00:00
-#SBATCH --mem=100G
+#SBATCH --mem=200G
 
-srun julia --threads 128 step2_autom_reduction_10.jl
+srun julia --threads 64 step2_autom_reduction_10.jl
