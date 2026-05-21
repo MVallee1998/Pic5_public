4a4a72a906e618316d87a574f4879654b8245d63
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-05-21 22:49:13 +0200

    retrying step2

--- Picard_5/submit_step2.sh
+++ Picard_5/submit_step2.sh
@@ -5,8 +5,8 @@
 #SBATCH --error=step2_7-10.err
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=1
 #SBATCH --time=2-00:00
-#SBATCH --mem=200G
+#SBATCH --mem=100G
 
-srun julia --threads 64 step2_autom_reduction_10.jl
+srun julia step2_autom_reduction_10.jl
