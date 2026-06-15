2ba91f4cd52a18a292b937799450394e03770222
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-06-15 18:21:01 +0200

    before doing optimization

--- Picard_5/enumerate_pseudomanifolds_10_each_l.jl
+++ Picard_5/enumerate_pseudomanifolds_10_each_l.jl
@@ -1,5 +1,10 @@
 include("../enumerate_kernel.jl")
 
+using Base.Threads: @spawn
+using ThreadsX
+using Profile
+
+
 function build_finalDB_single_v_one_l!(pseudo_manifolds_DB::Dict{Int,Vector{Set{BitVector}}},
                                        mat_DB::Dict{Int,Vector{Vector{UInt32}}},
                                        iso_DB::Dict{Int,Dict{Int,Vector{Tuple{Int,Any}}}},
@@ -17,26 +22,30 @@ function build_finalDB_single_v_one_l!(pseudo_manifolds_DB::Dict{Int,Vector{Set{
     for (index_contraction, perm) in iso_DB[m][l]
         links = collect(pseudo_manifolds_DB[m-1][index_contraction])
 
-        @showprogress desc="l=$(l), links=$(length(links))" for L_bit in links
+        # Precompute all mandatory_facets_bit upfront (cheap, serial)
+        prepared = Vector{BitVector}(undef, length(links))
+        for (i, L_bit) in pairs(links)
             mandatory_facets_bin = relabel(mat_DB[m-1][index_contraction][findall(L_bit)], perm)
-            mandatory_facets_bit = subset_bitvector(bases_bin, mandatory_facets_bin)
+            prepared[i] = subset_bitvector(bases_bin, mandatory_facets_bin)
+        end
 
-            if count(mandatory_facets_bit) != length(mandatory_facets_bin)
-                @warn "Some mandatory facets not found!" m l mandatory_facets_bin
-            end
-
+        @showprogress dt=0.5 desc="l=$(l), links=$(length(links))" for mandatory_facets_bit in prepared
             all_solutions_bit = enumerate_kernel_with_constraints_bitvector(A, kernel_basis, mandatory_facets_bit)
 
-            for K_bit in all_solutions_bit
+            solutions = collect(all_solutions_bit)
+            tasks = [@spawn begin
                 facets_bin = compl_bases_bin[findall(K_bit)]
-                if euler_sphere_test(facets_bin)
-                    push!(result, copy(K_bit))
-                end
+                euler_sphere_test(facets_bin) ? copy(K_bit) : nothing
+            end for K_bit in solutions]
+
+            for t in tasks
+                r = fetch(t)
+                r !== nothing && push!(result, r)
             end
         end
     end
 
-    open("results/pseudo_manifolds_10_part_$(l).jls", "w") do io
+    open("results/pseudo_manifolds_10_part_$(l)_test.jls", "w") do io
         serialize(io, result)
     end
 
@@ -51,4 +60,5 @@ mat_DB = open("resources/mat_DB.jls", "r") do io deserialize(io) end
 iso_DB = open("resources/iso_DB.jls", "r") do io deserialize(io) end
 pseudo_manifolds_DB = open("results/pseudo_manifolds_7-9_all.jls", "r") do io deserialize(io) end
 
-build_finalDB_single_v_one_l!(pseudo_manifolds_DB, mat_DB, iso_DB, m, l)
+@profile build_finalDB_single_v_one_l!(pseudo_manifolds_DB, mat_DB, iso_DB, m, l)
+Profile.print(mincount=50)
