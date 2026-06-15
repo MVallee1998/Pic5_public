844aaf4ab38ccc3bd01103814c7d236b0757d302
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-06-15 16:58:23 +0200

    better resources for nic5

--- Picard_5/submit_10_enum_each_l.sh
+++ Picard_5/submit_10_enum_each_l.sh
@@ -4,12 +4,12 @@
 #SBATCH --output=logs/res_Pic5_10_%a.out
 #SBATCH --error=logs/res_Pic5_10_%a.err
 #
-#SBATCH --array=1-46
+#SBATCH --array=16-46
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=32
 #SBATCH --time=2-00:00
-#SBATCH --mem=100G
+#SBATCH --mem=16G
 
 mkdir -p logs results
 
-srun julia --threads 64 enumerate_pseudomanifolds_10_each_l.jl $SLURM_ARRAY_TASK_ID
+srun julia --threads 32 enumerate_pseudomanifolds_10_each_l.jl $SLURM_ARRAY_TASK_ID
