399dc6ba0a4885a6f4553dac01e24bcb8075507f
Mathieu Vallée <mathieu.vallee1998@gmail.com>
2026-06-19 11:30:54 +0200

    changing server setup

--- Picard_5/submit_step2.sh
+++ Picard_5/submit_step2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-#SBATCH --job-name=Pic5_10
+#SBATCH --job-name=step2
 #SBATCH --output=logs/step2_7-10.out
 #SBATCH --error=logs/step2_7-10.err
 #
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=32
+#SBATCH --cpus-per-task=64
 #SBATCH --time=2-00:00
-#SBATCH --mem=100G
+#SBATCH --mem=250G
 
-srun julia --threads 32 step2_autom_reduction_10.jl
+srun julia --threads 64 step2_autom_reduction_10.jl
